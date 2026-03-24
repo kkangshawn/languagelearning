d4473901c51659565b437bbaa1efb9dd3068025c
Siwon Kang <kkangshawn@gmail.com>
2026-03-24 03:48:58 +0100

    test for google cloud run deployment

--- application.py
+++ application.py
@@ -1,15 +1,29 @@
-from languagelearning import create_app
+from flask import Flask
 import traceback
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("LanguageLearningApp")
 
 logger.info("--- Starting application... ---")
 try:
-    application = create_app()
+    app = Flask(__name__)
+    app.secret_key = "dev"
+
+    app.config.from_prefixed_env()
+
+    from languagelearning import auth
+    app.register_blueprint(auth.bp)
+
+    from languagelearning import posts
+    app.register_blueprint(posts.bp)
+    app.add_url_rule('/', endpoint='index_posts')
+
+    from languagelearning import wordbook
+    app.register_blueprint(wordbook.bp)
+
     logger.info("--- Application created successfully. ---")
-    app = application
 except Exception as e:
     print(f"Error running application: {e}")
     traceback.print_exc()
@@ -17,4 +31,5 @@ except Exception as e:
     raise e
 
 if __name__ == "__main__":
-    application.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
